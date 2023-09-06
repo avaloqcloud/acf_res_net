a77bac30553699fa82fe53f1909dcab69c99ee78
Andreas Kaderli <andreas.kaderli@avaloq.com>
2023-09-06 13:26:01 +0200

    vcn resource with dependency to module.configuration

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 ##Nested Modules
-module "vcn" {
-  compartment_id = var.compartment_id
-  source         = "./vcn"
-  vcns           = var.vcns
+# module "vcn" {
+#   # source         = "./vcn"
+#   compartment_id = module.configuration.setting.compartment_id
+#   display_name = lower("${moduel.configuration.setting.name}_vcn")
+#   dns_label = lower("${moduel.configuration.setting.name}")
+  
+#   depends_on = [ module.configuration ]
+# }s
+
+
+# Create VCNs
+resource "oci_core_vcn" "these" {
+  compartment_id = module.configuration.setting.compartment_id
+  display_name = lower("${moduel.configuration.setting.name}_vcn")
+  dns_label = lower("${moduel.configuration.setting.name}")
+  
+  depends_on = [ module.configuration ]
 }
