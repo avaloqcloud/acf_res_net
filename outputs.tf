9b4ff2ce5dfa0e397bed31e4298129d1c9b71d10
Reece Bailey <Riice@users.noreply.github.com>
2023-09-07 14:48:57 +0200

    Correct outputs formatting

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vcns" {
     description = "VCN Info"
-    value = {​​​​for vcn in oci_core_vcn.these : vcn.display_name
+    value = {​​​​for vcn in oci_core_vcn.these : vcn.display_name}
 }
