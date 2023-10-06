c4e5cc0ae48f832ba2f42ed6a62dd1c30a63c040
Andreas Kaderli <andreas.kaderli@avaloq.com>
2023-10-06 15:58:03 +0200

    fix wrong port assignemnt for ICMP ingress rules

--- vcn/main.tf
+++ vcn/main.tf
@@ -771,8 +771,8 @@ resource "oci_core_security_list" "these" {
       for x in each.value.traffic_rules["ingress"] != null ? each.value.traffic_rules["ingress"] : local.default_security_list_opt.ingress_rules :
       {
         proto : x.protocol
-        dst : x.dst
-        dst_type : x.dst_type
+        src : x.src
+        src_type : x.src_type
         stateless : x.stateless
         description : x.description
       } if x.protocol == "1" && x.icmp_type == null && x.icmp_code == null
